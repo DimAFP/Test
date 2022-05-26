edd2d04c1170999c00aec8662a970e8ef949974f
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-26 10:19:01 +0200

    DimView

---
+++ Test/DimView.swift
@@ -0,0 +1,20 @@
+//
+//  DimView.swift
+//  Test
+//
+//  Created by dimitri on 26/05/2022.
+//
+
+import SwiftUI
+
+struct DimView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DimView_Previews: PreviewProvider {
+    static var previews: some View {
+        DimView()
+    }
+}
